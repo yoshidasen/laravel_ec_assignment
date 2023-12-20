84485287679c0efcadddd962808dd03d78f02c81
yoshida <c.yoshida.sys22@morijyobi.ac.jp>
2023-12-20 12:31:43 +0900

    注文テーブルマイグレーションの作成 マイグレーションの作成 #2

---
+++ database/migrations/2023_12_20_032437_create_oders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->nullable(false);
+            $table->foreignId('address_id')->constrained('addresses')->nullable(false);
+            $table->string('payment_method')->nullable(false);
+            $table->foreignId('order_status')->constrained('oder_statuses')->default(01);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oders');
+    }
+};
