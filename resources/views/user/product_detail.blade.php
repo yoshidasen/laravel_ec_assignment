13bb71f3c98be1d24d1588b7754f40350812ad0b
yoshida <c.yoshida.sys22@morijyobi.ac.jp>
2024-01-25 11:32:34 +0900

    formの作成 カート機能の作成 #13

--- resources/views/user/product_detail.blade.php
+++ resources/views/user/product_detail.blade.php
@@ -7,7 +7,14 @@
     <p>{{$items->name}}</p>
     <p style="background: gold; width: 80px; text-align: center; border-radius: 10px;">{{$items->category->name}}</p>
     <img src="" alt="{{$items->name}}">
-    <p>{{$items->price}}<small>税込み</small></p>
+    <p>{{$items->price}}<small>税抜き{{$items->price / 1.1}}</small></p>
+    <form action="/user/product_detail/{{$items->id}}" method="post">
+        <input type="hidden" name="id" value="{{$items->id}}">
+        <input type="hidden" name="name" value="{{$items->name}}">
+        <input type="hidden" name="quantity" value="{{$items->quantity}}">
+        <input type="hidden" name="price" value="{{$items->price}}">
+        <input type="submit" value="カートに入れる">
+    </form>
     <p style="white-space:pre-wrap; background: white; padding: 10px;">{{$items->explanation}}</p>
 
 
